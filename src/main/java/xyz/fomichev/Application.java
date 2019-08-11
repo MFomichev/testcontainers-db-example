cd08569509e3a35c804734514c3297390f189e00
Mikhail Fomichev <mikhail@fomichev.xyz>
2019-08-11 11:10:02 +0300

    Init commit

---
+++ src/main/java/xyz/fomichev/Application.java
@@ -0,0 +1,12 @@
+package xyz.fomichev;
+
+/**
+ * @author Mikhail Fomichev (MFomichev)
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
